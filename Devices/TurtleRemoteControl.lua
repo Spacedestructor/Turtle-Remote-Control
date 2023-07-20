946c40ac8d322d3e9154ad8e7fa3d9a0ae0a65ae
Spacedestructor <Spacedestructor@users.noreply.github.com>
2023-07-20 19:22:15 +0200

    loading and saving
    the websocket will no attempt to retrieve  device data from local file "Devices.json" before launching and write any changes directly to the file. failure to locate file will result in the server crashing. packet forwarding hasnt been fully implemented yet and for the time being will retrieve test data instead. this is not yet send back to the website as that part is depending in the implementation of packet forwarding.

--- Devices/TurtleRemoteControl.lua
+++ Devices/TurtleRemoteControl.lua
@@ -1,12 +1,18 @@
 ---@diagnostic disable: undefined-field
-Websocket = "ws://127.0.0.1:3000"
-Webserver = "ws://127.0.0.1:3001"
+Adress = "ws://127.0.0.1:3000"
 Type = "Turtle"
 Label = ""
 Modem = { present = false, side = "", wrap = "" }
 Profession = ""
 ID = os.getComputerID()
 BroadcastNetwork = "0"
+Tool = ""
+Fuel = {
+	max = turtle.getFuelLimit(),
+	current = turtle.getFuelLevel(),
+	name = "",
+	FuelPerItem = 0
+}
 Packet = { ["source"] = ID, ["destination"] = "server", ["data"] = "", ["type"] = "" }
 SerializedPacket = textutils.serializeJSON(Packet)
 --Get Peripherals
@@ -23,140 +29,152 @@ end
 assert(Modem.side == "left" or "right",
 	"We somehow failed to save what side the Modem is on, after defining what side it is?")
 Modem.wrap = peripheral.wrap(Modem.side)
+assert(Modem.wrap.isWireless(), "Modem has to be Wireless for this Script to work!")
 print("Modem Present: " .. tostring(Modem.present) .. ", Side: " .. Modem.side)
 --Hardcoded Instructions
-local websocket, err = assert(http.websocket(Websocket))
-while websocket do
+local websocket, err = assert(http.websocket(Adress))
+while true do
 	local event, url, message, binary = os.pullEvent()
 	if event == "websocket_message" then
 		message = textutils.unserializeJSON(message)
-		if message.destination == os.getComputerID() then
-			if message.type == "instruction" and message.data == "GetInventory" then
-			elseif message.type == "instruction" and message.data == "GetProfession" then
-				for i = 1, 16, 1 do
-					if turtle.getItemCount(i) > 0 then
-						ItemDetail = textutils.serializeJSON(turtle.getItemDetail(i, true))
-						if string.match(ItemDetail.name, "diamond_shovel") ~= nil then
-							turtle.select(i)
-							if peripheral.ispresent("right") and not peripheral.ispresent("left") then
-								Status, Reason = turtle.equipRight()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "digging"
-								end
-							elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
-								Status, Reason = turtle.equipLeft()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "digging"
-								end
+		if message.type == "GetDeviceData" then
+			local DeviceData = {
+				Label,
+				Profession,
+				Tool,
+				Modem,
+				BroadcastNetwork,
+				Fuel,
+				Type
+			}
+			Text = textutils.serializeJSON(DeviceData)
+			textutils.slowPrint(Text, 5)
+		elseif message.type == "GetProfession" then
+			for i = 1, 16, 1 do
+				if turtle.getItemCount(i) > 0 then
+					ItemDetail = textutils.serializeJSON(turtle.getItemDetail(i, true))
+					if string.match(ItemDetail.name, "diamond_shovel") ~= nil then
+						turtle.select(i)
+						if peripheral.ispresent("right") and not peripheral.ispresent("left") then
+							Status, Reason = turtle.equipRight()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Digging"
 							end
-						elseif string.match(ItemDetail.name, "crafting_table") ~= nil then
-							turtle.select(i)
-							if peripheral.ispresent("right") and not peripheral.ispresent("left") then
-								Status, Reason = turtle.equipRight()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "crafty"
-								end
-							elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
-								Status, Reason = turtle.equipLeft()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "crafty"
-								end
+						elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
+							Status, Reason = turtle.equipLeft()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Digging"
 							end
-						elseif string.match(ItemDetail.name, "diamond_axe") ~= nil then
-							turtle.select(i)
-							if peripheral.ispresent("right") and not peripheral.ispresent("left") then
-								Status, Reason = turtle.equipRight()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "felling"
-								end
-							elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
-								Status, Reason = turtle.equipLeft()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "felling"
-								end
+						end
+					elseif string.match(ItemDetail.name, "crafting_table") ~= nil then
+						turtle.select(i)
+						if peripheral.ispresent("right") and not peripheral.ispresent("left") then
+							Status, Reason = turtle.equipRight()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Crafty"
 							end
-						elseif string.match(ItemDetail.name, "diamond_hoe") ~= nil then
-							turtle.select(i)
-							if peripheral.ispresent("right") and not peripheral.ispresent("left") then
-								Status, Reason = turtle.equipRight()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "farming"
-								end
-							elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
-								Status, Reason = turtle.equipLeft()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "farming"
-								end
+						elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
+							Status, Reason = turtle.equipLeft()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Crafty"
 							end
-						elseif string.match(ItemDetail.name, "diamond_pickaxe") ~= nil then
-							turtle.select(i)
-							if peripheral.ispresent("right") and not peripheral.ispresent("left") then
-								Status, Reason = turtle.equipRight()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "mining"
-								end
-							elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
-								Status, Reason = turtle.equipLeft()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "mining"
-								end
+						end
+					elseif string.match(ItemDetail.name, "diamond_axe") ~= nil then
+						turtle.select(i)
+						if peripheral.ispresent("right") and not peripheral.ispresent("left") then
+							Status, Reason = turtle.equipRight()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Felling"
 							end
-						elseif string.match(ItemDetail.name, "diamond_sword") ~= nil then
-							turtle.select(i)
-							if peripheral.ispresent("right") and not peripheral.ispresent("left") then
-								Status, Reason = turtle.equipRight()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "melee"
-								end
-							elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
-								Status, Reason = turtle.equipLeft()
-								if not Status then
-									print(Reason)
-								else
-									Profession = "melee"
-									os.setComputerLabel = tostring(ID) .. Profession
-								end
+						elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
+							Status, Reason = turtle.equipLeft()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Felling"
+							end
+						end
+					elseif string.match(ItemDetail.name, "diamond_hoe") ~= nil then
+						turtle.select(i)
+						if peripheral.ispresent("right") and not peripheral.ispresent("left") then
+							Status, Reason = turtle.equipRight()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Farming"
+							end
+						elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
+							Status, Reason = turtle.equipLeft()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Farming"
+							end
+						end
+					elseif string.match(ItemDetail.name, "diamond_pickaxe") ~= nil then
+						turtle.select(i)
+						if peripheral.ispresent("right") and not peripheral.ispresent("left") then
+							Status, Reason = turtle.equipRight()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Mining"
+							end
+						elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
+							Status, Reason = turtle.equipLeft()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Mining"
+							end
+						end
+					elseif string.match(ItemDetail.name, "diamond_sword") ~= nil then
+						turtle.select(i)
+						if peripheral.ispresent("right") and not peripheral.ispresent("left") then
+							Status, Reason = turtle.equipRight()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Melee"
+							end
+						elseif peripheral.isPresent("left") and not peripheral.ispresent("right") then
+							Status, Reason = turtle.equipLeft()
+							if not Status then
+								print(Reason)
+							else
+								Profession = "Melee"
+								os.setComputerLabel = tostring(ID) .. Profession
 							end
 						end
 					end
 				end
-				Packet = { ["source"] = ID, ["destination"] = "server", ["data"] = Profession, ["type"] = "GetProfession-Response" }
-				SerializedPacket = textutils.serializeJSON(Packet)
-				websocket.send(SerializedPacket)
 			end
+			Packet = {
+				["source"] = ID,
+				["destination"] = "server",
+				["data"] = Profession,
+				["type"] = "GetProfession-Response"
+			}
+			SerializedPacket = textutils.serializeJSON(Packet)
+			websocket.send(SerializedPacket)
 		end
 	elseif event == "websocket_closed" then
 		print(event .. ", the websocket to " .. url .. " closed. Reason: " .. message .. " (" .. binary .. ")")
-		websocket, err = assert(http.websocket(Websocket))
+		os.reboot()
 	end
 end
 --Old Part
-os.setComputerLabel(Label .. " " .. tostring(ID))
-print("Label: " .. Label)
 --Join Network
-assert(Modem.isWireless, "We cant work with a wired Modem, replace it with a Whireless Modem!")
 Modem.open(BroadcastNetwork)
 Modem.transmit("Device " .. Label .. "(" .. ID .. ")" .. " joined the Broadcast Network!")
 print("Joined Broadcast Network " .. BroadcastNetwork)
--- websocket.js
+++ websocket.js
@@ -50,59 +50,112 @@
 //in both cases update as the respective commands are used.
 //a turtle should always start as an empty advanced turtle with wireless modem peripheral.
 //all required items (fuel, tools etc.) should be in the turtles inventory.
-/*
-Get Turtle Peripherals
-Set Profession
-function GetMaxFuelLevel() {}
-function GetCurrentFuelLevel() {}
-function Refuel() {}
-function getInventory() {}
-function GetHeight() {}
-function Box() {}
-function ParseBlockID(BlockID) { var parsed = { source : "", material : "", variant : ""} if (BlockID.includes(":") && BlockID("_")) { parsed.source = BlockID.slice(0, BlockID.indexOf(":")-1) return parsed} else {return "Invalid Block ID"}}
-*/
-const hostname = "127.0.0.1"
-const port = 3000
+//Get Turtle Peripherals
+//Set Profession
+//function GetMaxFuelLevel() {}
+//function GetCurrentFuelLevel() {}
+//function Refuel() {}
+//function getInventory() {}
+//function GetHeight() {}
+//function Box() {}
+//function ParseBlockID(BlockID) { var parsed = { source : "", material : "", variant : ""} if (BlockID.includes(":") && BlockID("_")) { parsed.source = BlockID.slice(0, BlockID.indexOf(":")-1) return parsed} else {return "Invalid Block ID"}}
+const hostName = "127.0.0.1"
+const hostPort = 3000
 const socket = require("ws")
-
-const WebServerIP = "127.0.0.1"
-const WebServerPort = "3000"
-
-var WebSocket = new socket.Server({ port: port })
-var turtles = require("./Turtle.json")
-//var computers = require("./ComputerData.json")
-//var pockets = require("./PocketData.json")
-
-WebSocket.on("connection", (WSClient, WSRequest) => {
-	console.info(`Web Socket: Incoming Connection from ${WSRequest.socket.remoteAddress}:${WSRequest.socket.remotePort}`)
-	WSClient.on("message", serializedrequest => {
-		var IP = WSClient._socket.remoteAddress
-		var Port = WSClient._socket.remotePort
-		var request = JSON.parse(serializedrequest)
-		console.error(request)
-		switch (request.type) {
-			case "GetProfession":
-				Packet = { ["source"]: "server", ["destination"]: ID }
-				break
-			case "GetProfession-Response":
-				break
-			case "GetTurtleData":
-				var ID = Number(request.data)
-				var TurtleData = turtles[ID]
-				console.log(TurtleData)
-				//WSClient.send(Packet)
-				break
-			default:
-				if (request.type != undefined) {
-					console.error(`Unknown Packet Type: ${request.type}! ${serializedrequest}`)
-				} else {
-					console.error(`Unknown Packet structure: ${serializedrequest}`)
+var webSocket = new socket.Server({ host: hostName, port: hostPort })
+var devices = require("./Devices.json")
+const fs = require("fs")
+//Parameter must be a File as string. Will return retrieved data without any further Processing.
+function load_from_file(file) {
+	try {
+		var data = fs.readFileSync(file)
+		return data
+	} catch (error) {
+		console.error(error)
+		throw error
+	}
+}
+//First Parameter must be a File as String, Second Parameter must be the data to be saved. Wont do any further Processing before saving. Will return true if operation was successful.
+function write_to_file(file, data) {
+	try {
+		fs.writeFileSync(file, data)
+		return true
+	} catch (error) {
+		console.error(error)
+		throw error
+	}
+}
+devices = load_from_file("Devices.json")
+if (Object.keys(devices).length > 0) {
+	devices = JSON.parse(devices)
+	console.debug(devices)
+} else {
+	console.info("Devices List Empty!")
+}
+var clients = []
+var requestQueue = {}
+//Creates a 128-bit value. Taken from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
+function create_UUID() {
+	var dt = new Date().getTime();
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = (dt + Math.random() * 16) % 16 | 0;
+		dt = Math.floor(dt / 16);
+		return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+	});
+	return uuid;
+}
+function HandlePackets(serializedRequest, wsClient, wsRequest) {
+	var request = JSON.parse(serializedRequest.toString())
+	console.debug(request)
+	switch (request.type) {
+		case "GetDeviceData":
+			//var request = { [] }
+			//requestQueue = requestQueue + request
+			//console.debug(requestQueue)
+			var deviceID = Number(request.data)
+			var deviceData = devices[deviceID]
+			var packet = { ["source"]: "server", ["destination"]: "server", ["data"]: deviceData, ["type"]: "GetTurtleData-Response" }
+			packet = JSON.stringify(packet)
+			console.debug(packet)
+			wsClient.send(packet)
+			break
+		case "Identify":
+			//Example JSON: "UUID": { "ip": "127.0.0.1:3002", "type": "Client", "id": "" }
+			if (request.source == "client") {
+				var client = { ["uuid"]: create_UUID(), ["ip"]: wsRequest.socket.remoteAddress, ["port"]: wsRequest.socket.remotePort }
+				clients.push(client)
+				console.debug(clients)
+				//deviceList = JSON.parse(devices)
+				console.debug(Object.keys(devices).length)
+				if (Object.keys(devices).length == 0) {
+					var deviceList = { "0": { "label": "Test Computer", "modem": { "side": "right", "present": true, "type": "Wireless" }, "broadcastNetwork": 0, "type": "Computer", "id": 0 }, "4": { "label": "Test Pocket", "modem": { "side": "right", "present": true, "type": "Wireless" }, "broadcastNetwork": 0, "type": "Pocket", "id": 4 }, "5": { "label": "Test Turtle", "profession": "Miner", "tool": "minecraft:diamond_pickaxe", "modem": { "side": "right", "present": true, "type": "Wireless" }, "broadcastNetwork": 0, "fuel": { "max": 0, "current": 0, "name": "minecraft:coal", "fuelPerItem": 0 }, "type": "Turtle", "id": 5 } }
+					devices = JSON.stringify(deviceList)
+					write_to_file("Devices.json", devices)
 				}
-				break
-		}
+				console.debug(devices)
+				console.debug(JSON.parse(devices))
+				console.debug(Object.keys(devices).length)
+				//console.debug(devicelist)
+				//var packet = {}
+				//wsClient.send()
+			}
+			break
+		default:
+			if (request.type != undefined) {
+				console.error(`Unknown Packet Type: ${request.type}! ${serializedRequest}`)
+			} else {
+				console.error(`Unknown Packet structure: ${serializedRequest}`)
+			}
+			break
+	}
+}
+webSocket.on("connection", (wsClient, wsRequest) => {
+	console.info(`Web Socket: Incoming Connection from ${wsRequest.socket.remoteAddress}:${wsRequest.socket.remotePort}`)
+	wsClient.on("message", function (serializedRequest) {
+		HandlePackets(serializedRequest, wsClient, wsRequest)
 	})
-	WSClient.on("close", (event) => {
-		console.log(`Web Socket: Connection to ${IP}:${Port} Terminated!`)
+	wsClient.on("close", (event) => {
+		console.info(`Web Socket: Connection to ${ip}:${port} Terminated!`)
 	})
 })
-console.log(`Web Socket Running at http://${hostname}:${port}`)
+console.info(`Web Socket Running at http://${hostName}:${hostPort}`)

40f33fba8ebb56e74f9ab1d59f32e805fe329365
Spacedestructor <Spacedestructor@users.noreply.github.com>
2023-08-29 14:26:10 +0200

    Web Interface Data Request Update
    Web Interface is now able to request either all devices, all data of a specific device or any specific data set of a specific device. Buttons SHOULD enable and disable as the respective features become available or unavable but this feature hasnt been fully tested yet. Buttons are on page load initiated to there default states. Web Interface will automatically in chain request first identification followed by a request of all devices upon connection. The javascript version of the websocket has been updated alongside while the C# rewrite is still in process. The Websocket will now only assign a new identification if the IP hasnt been identified in the current session before and thus eliminate any duplicate issues. The websocket will for now serve dummy data as the work on the devices has just begun. Removed old Prototype device scripts as they are all getting major reworks, also removed currently unused Files "minecraft.json", "computercraft.json" and "todo.txt". The first two because they are currently not used until the 3D rendering gets implemented and the last one because its a dublicate of both my local "To-do.md" abd the Wiki´s To-do List. Currently there is no error handling during the phase of identification, if a connecting Web Interface or Device doesnt follow the Protocol exactly it will crash the Websocket. A Fix to this will be included with either the 3D Rendering Update or the Instruction set/chain update later after the device scripts have catched up to the progression of the web interface and the websocket.

--- websocket.js
+++ websocket.js
@@ -42,36 +42,21 @@
 //inspectUp()
 //inspectDown()
 //getItemDetail(slot, detailed)
-//"Content-Type: application/json"
-//0.0.0.0:0
-//255.255.255.255:65536
-//Check if the Turtle is already Registered.
-//if yes set web ui to last known info, if no keep web ui empty until the data has been retrieved from the turtle.
-//in both cases update as the respective commands are used.
-//a turtle should always start as an empty advanced turtle with wireless modem peripheral.
-//all required items (fuel, tools etc.) should be in the turtles inventory.
-//Get Turtle Peripherals
-//Set Profession
-//function GetMaxFuelLevel() {}
-//function GetCurrentFuelLevel() {}
-//function Refuel() {}
-//function getInventory() {}
-//function GetHeight() {}
-//function Box() {}
-//function ParseBlockID(BlockID) { var parsed = { source : "", material : "", variant : ""} if (BlockID.includes(":") && BlockID("_")) { parsed.source = BlockID.slice(0, BlockID.indexOf(":")-1) return parsed} else {return "Invalid Block ID"}}
 const hostName = "127.0.0.1"
 const hostPort = 3000
 const socket = require("ws")
 var webSocket = new socket.Server({ host: hostName, port: hostPort })
 var devices = require("./Devices.json")
 const fs = require("fs")
+const { assert } = require("console")
+const { isNumberObject } = require("util/types")
 //Parameter must be a File as string. Will return retrieved data without any further Processing.
 function load_from_file(file) {
 	try {
 		var data = fs.readFileSync(file)
 		return data
 	} catch (error) {
-		console.error(error)
+		console.error("Error: ", error)
 		throw error
 	}
 }
@@ -81,81 +66,179 @@ function write_to_file(file, data) {
 		fs.writeFileSync(file, data)
 		return true
 	} catch (error) {
-		console.error(error)
+		console.error("Error: ", error)
 		throw error
 	}
 }
 devices = load_from_file("Devices.json")
 if (Object.keys(devices).length > 0) {
 	devices = JSON.parse(devices)
-	console.debug(devices)
+	console.log("Web-socket: Device List loaded successfully! ", devices)
 } else {
-	console.info("Devices List Empty!")
+	console.log("Devices List Empty! ", devices)
 }
 var clients = []
-var requestQueue = {}
 //Creates a 128-bit value. Taken from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
 function create_UUID() {
 	var dt = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+	var uuid = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
 		var r = (dt + Math.random() * 16) % 16 | 0;
 		dt = Math.floor(dt / 16);
 		return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
 	});
 	return uuid;
 }
+function check_client_existence(ip, port) {
+	var index = 0
+	if (Object.keys(clients).length > 0) {
+		while (index <= Object.keys(clients).length - 1) {
+			if (ip == clients[index].ip) {
+				console.debug(clients[index])
+				return clients[index]
+			}
+			index++
+		}
+		return false
+	} else {
+		return false
+	}
+}
 function HandlePackets(serializedRequest, wsClient, wsRequest) {
-	var request = JSON.parse(serializedRequest.toString())
-	console.debug(request)
-	switch (request.type) {
-		case "GetDeviceData":
-			//var request = { [] }
-			//requestQueue = requestQueue + request
-			//console.debug(requestQueue)
-			var deviceID = Number(request.data)
-			var deviceData = devices[deviceID]
-			var packet = { ["source"]: "server", ["destination"]: "server", ["data"]: deviceData, ["type"]: "GetTurtleData-Response" }
-			packet = JSON.stringify(packet)
-			console.debug(packet)
-			wsClient.send(packet)
-			break
-		case "Identify":
-			//Example JSON: "UUID": { "ip": "127.0.0.1:3002", "type": "Client", "id": "" }
-			if (request.source == "client") {
-				var client = { ["uuid"]: create_UUID(), ["ip"]: wsRequest.socket.remoteAddress, ["port"]: wsRequest.socket.remotePort }
+	var request = JSON.parse(serializedRequest)
+	var code = request.code
+	var data = request.data
+	switch (code) {
+		case 100: //User lets us know it wants to be Identified.
+			var ip = wsRequest.socket.remoteAddress
+			var port = wsRequest.socket.remotePort
+			var client = check_client_existence(ip, port)
+			if (client == false) {
+				//Example JSON: 0: { "ip": "127.0.0.1:3002", "type": "Client", "uuid": "example-uuid" }
+				client = { ["uuid"]: create_UUID(), ["ip"]: ip, ["port"]: port }
+				//console.debug("Client: ", client)
 				clients.push(client)
-				console.debug(clients)
-				//deviceList = JSON.parse(devices)
-				console.debug(Object.keys(devices).length)
-				if (Object.keys(devices).length == 0) {
-					var deviceList = { "0": { "label": "Test Computer", "modem": { "side": "right", "present": true, "type": "Wireless" }, "broadcastNetwork": 0, "type": "Computer", "id": 0 }, "4": { "label": "Test Pocket", "modem": { "side": "right", "present": true, "type": "Wireless" }, "broadcastNetwork": 0, "type": "Pocket", "id": 4 }, "5": { "label": "Test Turtle", "profession": "Miner", "tool": "minecraft:diamond_pickaxe", "modem": { "side": "right", "present": true, "type": "Wireless" }, "broadcastNetwork": 0, "fuel": { "max": 0, "current": 0, "name": "minecraft:coal", "fuelPerItem": 0 }, "type": "Turtle", "id": 5 } }
-					devices = JSON.stringify(deviceList)
-					write_to_file("Devices.json", devices)
-				}
-				console.debug(devices)
-				console.debug(JSON.parse(devices))
-				console.debug(Object.keys(devices).length)
-				//console.debug(devicelist)
-				//var packet = {}
-				//wsClient.send()
+				//console.debug("Clients: ", clients)
+				var packet = { ["code"]: code, ["data"]: client }
+				var serializedPacket = JSON.stringify(packet)
+				wsClient.send(serializedPacket)
+			} else {
+				console.log(`Client has already registered as ${JSON.stringify(client)}!`)
+				var packet = { ["code"]: code, ["data"]: client }
+				var serializedPacket = JSON.stringify(packet)
+				wsClient.send(serializedPacket)
 			}
 			break
-		default:
+		case 101: //User lets us know what Cookie in the Headers of this Connection has been Modified.
+			console.error(`${code} not yet implemented!`)
+			break
+		case 199: //User lets us know it has disconnected.
+			var remoteAddress = wsRequest.connection.remoteAddress
+			var remotePort = wsRequest.connection.remotePort
+			var uuid
+			let i = 0
+			while (i < clients.length) {
+				var client = clients[i]
+				if (client.ip == remoteAddress && client.port == remotePort) {
+					uuid = clients[i].uuid
+					//console.debug("Clients: ", clients)
+					clients.splice(i, 1)
+					//console.debug("Clients: ", clients)
+				}
+				i++
+			}
+			console.log("Client" + uuid + "from" + remoteAddress + ":" + remotePort + " has disconnected!")
+			break
+		case 300: //User lets us know what Twitch/Youtube/Other Platform Account they have logged in to. This will be Platform Name and UserID of the Platform.
+			break
+		case 301: //User is requesting all Data of all Devices it has access to.
+			//console.debug(`Devices: `, devices)
+			var packet = { ["code"]: code, ["data"]: devices }
+			//console.debug("Packet: ", packet)
+			var serializedPacket = JSON.stringify(packet)
+			//console.debug("Serialized Packet: ", serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 302: //User is requesting all Data of a specific Device.
+			//console.debug(`Device: `, devices[data])
+			var packet = { ["code"]: code, ["data"]: devices[data] }
+			//console.debug("Packet: ", packet)
+			var serializedPacket = JSON.stringify(packet)
+			//console.debug("Serialized Packet: ", serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 303: //User is requesting the Type of a specific Device.
+			//console.debug(`Device Type: `, devices[data].type)
+			var packet = { ["code"]: code, ["data"]: devices[data].type }
+			//console.debug(`Packet: `, packet)
+			var serializedPacket = JSON.stringify(packet)
+			//console.debug(`Serialized Packet: `, serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 304: //User is requesting the Label of a specific Device.
+			console.debug(`Device Label: `, devices[data].label)
+			var packet = { ["code"]: code, ["data"]: devices[data].label }
+			console.debug(`Packet: `, packet)
+			var serializedPacket = JSON.stringify(packet)
+			console.debug(`Serialized Packet: `, serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 305: //User is requesting the Fuel of a specific Device.
+			console.debug(`Device Fuel: `, devices[data].fuel)
+			var packet = { ["code"]: code, ["data"]: devices[data].fuel }
+			console.debug(`Packet: `, packet)
+			var serializedPacket = JSON.stringify(packet)
+			console.debug(`Serialized Packet: `, serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 306: //User is requesting the Modem of a specific Device.
+			console.debug(`Device Modem: `, devices[data].modem)
+			var packet = { ["code"]: code, ["data"]: devices[data].modem }
+			console.debug(`Packet: `, packet)
+			var serializedPacket = JSON.stringify(packet)
+			console.debug(`Serialized Packet: `, serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 307: //User is requesting the Tool of a specific Device.
+			console.debug(`Device Tool: `, devices[data].tool)
+			var packet = { ["code"]: code, ["data"]: devices[data].tool }
+			console.debug(`Packet: `, packet)
+			var serializedPacket = JSON.stringify(packet)
+			console.debug(`Serialized Packet: `, serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		case 308: //User is requesting the Braodcast Network of a specific Device.
+			console.debug(`Device Broadcast Network: `, devices[data].broadcastNetwork)
+			var packet = { ["code"]: code, ["data"]: devices[data].broadcastNetwork }
+			console.debug(`Packet: `, packet)
+			var serializedPacket = JSON.stringify(packet)
+			console.debug(`Serialized Packet: `, serializedPacket)
+			wsClient.send(serializedPacket)
+			break
+		default: //Logs all unknown Packet Codes and Structures.
 			if (request.type != undefined) {
-				console.error(`Unknown Packet Type: ${request.type}! ${serializedRequest}`)
+				console.error(`Unknown Packet Code: ${request.type}! ${serializedRequest}`)
 			} else {
-				console.error(`Unknown Packet structure: ${serializedRequest}`)
+				console.error(`Unknown Packet Structure: ${serializedRequest}`)
 			}
 			break
 	}
 }
 webSocket.on("connection", (wsClient, wsRequest) => {
-	console.info(`Web Socket: Incoming Connection from ${wsRequest.socket.remoteAddress}:${wsRequest.socket.remotePort}`)
+	console.log(`Web-socket: Incoming Connection from ${wsRequest.socket.remoteAddress}:${wsRequest.socket.remotePort}`)
 	wsClient.on("message", function (serializedRequest) {
 		HandlePackets(serializedRequest, wsClient, wsRequest)
 	})
 	wsClient.on("close", (event) => {
-		console.info(`Web Socket: Connection to ${ip}:${port} Terminated!`)
+		var result = false
+		var counter = 0
+		var uuid = ""
+		while (!result) {
+			if (clients[counter].ip == wsRequest.socket.remoteAddress && clients[counter].port == wsRequest.socket.remotePort) {
+				uuid = clients[counter].uuid
+				result = true
+			}
+		}
+		console.log(`Web-socket: Connection to ${uuid} from ${wsRequest.socket.remoteAddress}:${wsRequest.socket.remotePort} Terminated!`)
 	})
 })
-console.info(`Web Socket Running at http://${hostName}:${hostPort}`)
+console.log(`Web-socket Running at http://${hostName}:${hostPort}`)
